7da98e6183e15e2b2dce4a36c798233b66feae25
jiboxue <jibx@ichsy.com>
2016-11-17 11:21:06 +0800

    first commit

---
+++ PPX_LateralSpreads/Class/TwoViewController.h
@@ -0,0 +1,13 @@
+//
+//  TwoViewController.h
+//  PPX_LateralSpreads
+//
+//  Created by pipixia on 2016/11/9.
+//  Copyright © 2016年 pipixia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TwoViewController : UIViewController
+
+@end
---
+++ PPX_LateralSpreads/Class/TwoViewController.m
@@ -0,0 +1,37 @@
+//
+//  TwoViewController.m
+//  PPX_LateralSpreads
+//
+//  Created by pipixia on 2016/11/9.
+//  Copyright © 2016年 pipixia. All rights reserved.
+//
+
+#import "TwoViewController.h"
+
+@interface TwoViewController ()
+
+@end
+
+@implementation TwoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
